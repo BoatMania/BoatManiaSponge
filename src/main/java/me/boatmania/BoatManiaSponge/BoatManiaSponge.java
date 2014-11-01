27f9ac47a10d57a03e8b3220287e9428e01a2b9d
MrMysteri0us <MrMysteri0us@outlook.de>
2014-11-01 12:45:44 +0100

    Create proper plugin main class

--- src/main/java/me/boatmania/BoatManiaSponge/BoatManiaSponge.java
+++ src/main/java/me/boatmania/BoatManiaSponge/BoatManiaSponge.java
@@ -1,8 +1,23 @@
 package me.boatmania.BoatManiaSponge;
 
-public class BoatManiaSponge {
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.api.event.SpongeEventHandler;
+import org.spongepowered.api.event.state.PreInitializationEvent;
+import org.spongepowered.api.event.state.ServerStoppingEvent;
+import org.spongepowered.api.plugin.Plugin;
 
-    public static void main(String[] args) {
-        System.out.println("Hi");
+@Plugin(id = "BoatManiaSponge", name = "BoatManiaSponge", version = "1.0")
+public class BoatManiaSponge {
+    private Logger log;
+
+    @SpongeEventHandler
+    public void onInit(PreInitializationEvent event) {
+        log = event.getPluginLog();
+        log.info("BoatManiaSponge enabled.");
+    }
+
+    @SpongeEventHandler
+    public void onStop(ServerStoppingEvent event) {
+        log.info("BoatManiaSponge disabled.");
     }
 }
